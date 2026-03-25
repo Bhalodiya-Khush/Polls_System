d09de50ff485f7bdd1905494f556f9e3be633276
Bhalodiya-khush <bhalodiyakhush9@gmail.com>
2026-03-25 14:46:30 +0530

    tutorial 7

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-from .models import Question
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+
+admin.site.register(Question, QuestionAdmin)
